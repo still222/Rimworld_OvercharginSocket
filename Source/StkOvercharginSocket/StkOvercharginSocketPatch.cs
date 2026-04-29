40f0496df480e67e18774f94f2e39bc97ce5bc75
still222 <118853487+still222@users.noreply.github.com>
2026-04-29 14:28:36 +0500

    Main logic implementation
    While it is a very much working state, it desperately needs more optimization

--- Source/StkOvercharginSocket/StkOvercharginSocketPatch.cs
+++ Source/StkOvercharginSocket/StkOvercharginSocketPatch.cs
@@ -7,17 +7,25 @@ namespace StkOvercharginSocket;
 [HarmonyPatch(typeof(Building_MechCharger), nameof(Building_MechCharger.Tick))]
 public static class Patch_MechCharger_Tick
 {
+	private const float DefaultChargePerTick = 0.00083333335f;
 	public static void Prefix(Building_MechCharger __instance)
 	{
+		CompPowerLevel powerLevel = __instance.GetComp<CompPowerLevel>();
+
 		if (__instance.IsHashIntervalTick(250))
 		{
-			CompPowerLevel powerLevel = __instance.GetComp<CompPowerLevel>();
-
 			if (__instance.currentlyChargingMech != null && __instance.Power.PowerOn)
 				__instance.Power.PowerOutput = (0f - __instance.Power.Props.PowerConsumption) * powerLevel.PowerUsageMod;
 
 			else
 				__instance.Power.PowerOutput = 0f;
+		}
+
+		float chargePerTickMod = powerLevel.PowerLevel - 1f;
+		if (chargePerTickMod != 0f && __instance.currentlyChargingMech != null && __instance.Power.PowerOn)
+		{
+			__instance.currentlyChargingMech.needs.energy.CurLevel += chargePerTickMod * DefaultChargePerTick;
+			__instance.wasteProduced += __instance.WasteProducedPerTick * DefaultChargePerTick;
 		}
 	}
 }
